750c0dbb86b90769da6bcbb4a0c6274e54f11dc5
Kevin Ludwig <kevin.ludwig@gmail.com>
2025-01-27 09:42:17 -0800

    make easier to use

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 [![Build Status](https://travis-ci.org/kevinludwig/chess-opening-insights.svg?branch=master)](https://travis-ci.org/kevinludwig/chess-opening-insights)
 
-Analyze a PGN file to determine which openings I perform poorly in. E.g. where should I focus my opening study? Generally it's
-not much different than the lichess opening insight showing performance by ECO code. I'm planning on adding additional insights 
-that I can't get out of lichess.
+Analyze your chess.com openings to see where you're winning and losing
 
 ### Usage
 
+You need to modify the file `repertoire.js` to include the lines you want classified in your repertoire
+
 ```
-./insight.js my-games.pgn
+./insight.js --username <Chess.com username> --months <number of months of games to pull> --color <white or black>
 ```
 
 ### Output
@@ -17,7 +17,23 @@ that I can't get out of lichess.
 The result from the console log above would be 
 
 ```
-
+Other 43% / 9% / 48% (67)
+QGD Catalan 64% / 7% / 29% (14)
+Slav 57% / 14% / 29% (14)
+Modern 56% / 11% / 33% (9)
+Marshall Defense 60% / 0% / 40% (5)
+KID Fianchetto 100% / 0% / 0% (4)
+Englund Gambit 75% / 0% / 25% (4)
+Old Benoni 75% / 0% / 25% (4)
+Owens Defense 100% / 0% / 0% (3)
+Albin Counter Gambit 67% / 0% / 33% (3)
+Bogo Indian Defense 33% / 0% / 67% (3)
+Pirc 33% / 0% / 67% (3)
+Queens Indian Defense 100% / 0% / 0% (2)
+Benko Gambit 0% / 50% / 50% (2)
+Symmetrical Defense 100% / 0% / 0% (1)
+Budapest Gambit 100% / 0% / 0% (1)
+QGD Tarrasch Defense 100% / 0% / 0% (1)
 ```
 
 ### Run tests
---
+++ insight.mjs
@@ -0,0 +1,121 @@
+import util from 'node:util';
+import axios from 'axios';
+import pgnParser from 'pgn-parser';
+import ChessJs from 'chess.js';
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+import repertoire from './repertoire.js';
+
+const parser = await util.promisify(pgnParser)();
+const chess = new ChessJs.Chess();
+const optionsList = [
+    {name: 'help', alias: 'h', type: Boolean, description: 'get some help'},
+    {name: 'username', alias: 'u', type: String, description: 'Chess.com username', defaultValue: 'kevinthesnipe'},
+    {name: 'months', alias: 'm', type: Number, description: 'number of months of games to fetch', defaultValue: 3},
+    {name: 'color', alias: 'c', type: String, description: 'white or black', defaultValue: 'white'}
+];
+const options = commandLineArgs(optionsList);
+
+const gameResultMap = {
+    '1-0': 'win',
+    '0-1': 'loss',
+    '1/2-1/2': 'draw'
+};
+
+function winPercentage(w, l, d) {
+    const total = w + l + d;
+    const sw = Math.round((w / total).toPrecision(2) * 100);
+    const sl = Math.round((l / total).toPrecision(2) * 100);
+    const sd = Math.round((d / total).toPrecision(2) * 100);
+    return `${sw}% / ${sd}% / ${sl}%`;
+}
+
+const withPercentages = (obj) => Object.entries(obj).reduce((acc, [k, v]) => {
+    acc[k] = v;
+    acc[k].rate = winPercentage(v.win || 0, v.loss || 0, v.draw || 0);
+    return acc;
+}, {});
+
+function updateResult(result, line, gameResult) {
+    const key = line?.name ?? "Other";
+    
+    if (result[key]) {
+        result[key].total += 1;
+    } else {
+        result[key] = {total: 1};
+    }
+    
+    if (result[key][gameResult]) {
+        result[key][gameResult] += 1;
+    } else {
+        result[key][gameResult] = 1;
+    }
+}
+
+function printResult(result) {
+    Object.entries(result).sort((a, b) => {
+        const t1 = a[1].total;
+        const t2 = b[1].total;
+        if (t1 < t2) return 1;
+        else if (t1 > t2) return -1;
+        else return 0;
+    }).forEach(([k, v]) => {
+        console.log(k, v.rate, `(${v.total})`);  
+    });
+}
+
+async function lastNMonths(archives, username, color, n) {
+    let pgnData = "";
+    for (const url of archives.slice(-n)) {
+        const {data} = await axios.get(url);
+        pgnData += data.games
+            .filter(g => g.rules === 'chess' && g[color].username.toLowerCase() === username.toLowerCase())
+            .reduce((acc, cur) => acc.concat("", cur.pgn), "");
+    }
+    return pgnData;
+}
+
+async function fetchPgn(username, color, months) {
+    const {data} = await axios.get(`https://api.chess.com/pub/player/${username}/games/archives`);
+    const pgnData = await lastNMonths(data.archives, username, color, months);
+
+    /* calculate fen in each repertoire line */
+    for (const line of repertoire) {
+        const [g] = parser.parse(line.moves);
+        for (const {move} of g.moves) {
+            chess.move(move);
+        }
+        line.fen = chess.fen();
+        chess.reset();
+    }
+    return pgnData;
+}
+
+try {
+    if (options.help) {
+        console.log(commandLineUsage([
+            {header: 'Insights', content: 'Insight into opening variation results'},
+            {header: 'Options', optionList}
+        ]));
+    } else {
+
+        const pgnData = await fetchPgn(options.username, options.color, options.months);
+        const games = parser.parse(pgnData);
+        const result = {};
+        for (const game of games) {
+            let line = null;
+            for (const {move} of game.moves.slice(0, 20)) {
+                chess.move(move);
+                const l = repertoire.find(elem => elem.fen === chess.fen());
+                if (l) {
+                    line = l;
+                }
+            }
+            updateResult(result, line, gameResultMap[game.result]);
+            chess.reset();
+        }
+        printResult(withPercentages(result));
+    }
+} catch(ex) {
+    console.log(ex);
+}
