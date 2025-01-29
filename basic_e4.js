6fc337e17618244fe51ef79906dbd7aa6f184ee4
Kevin Ludwig <kevin.ludwig@gmail.com>
2025-01-28 20:03:54 -0800

    add basic e4 categorization

---
+++ basic_e4.js
@@ -0,0 +1,29 @@
+module.exports = {
+    white: [
+        {moves: "1. e4 e5 2. Nf3 Nf6 *", name: "Petroff"},
+        {moves: "1. e4 e5 2. Nf3 d6 *", name: "Philidor"},
+        {moves: "1. e4 e5 2. f4 *", name: "King's Gambit"},
+        {moves: "1. e4 e5 2. Nf3 f5 *", name: "Latvian Gambit"},
+        {moves: "1. e4 e5 2. Nf3 Nc6 3. d4 *", name: "Scotch Game"},
+        {moves: "1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5 4. b4 *", name: "Evan's Gambit"},
+        {moves: "1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5 *", name: "Italian Game"},
+        {moves: "1. e4 e5 2. Nf3 Nc6 3. Bb5 *", name: "Ruy Lopez"},
+        {moves: "1. e4 e5 2. Nf3 Nc6 3. Bc4 Nf6 *", name: "Two Knights"},
+        {moves: "1. e4 e5 2. Nc3 *", name: "Vienna Game"},
+        {moves: "1. e4 e5 2. d4 exd4 3. Qxd4 *", name: "Center Game"},
+        {moves: "1. e4 Nf6 *", name: "Alekhine's Defense"},
+        {moves: "1. e4 g6 2. d4 *", name: "Modern Defense"},
+        {moves: "1. e4 d6 2. d4 Nf6 *", name: "Pirc Defense"},
+        {moves: "1. e4 c6 2. d4 d5 *", name: "Caro Kann"},
+        {moves: "1. e4 c6 2. Nc3 *", name: "Caro Kann"},
+        {moves: "1. e4 d5 2. exd5 *", name: "Scandinavian"},
+        {moves: "1. e4 c5 *", name: "Sicilian Defense"},
+        {moves: "1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 *", name: "Sicilian: Najdorf"},
+        {moves: "1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 g6 *", name: "Sicilian: Dragon"},
+        {moves: "1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 e6 *", name: "Sicilian: Scheveningen"},
+        {moves: "1. e4 c5 2. Nf3 e6 3. d4 cxd4 4. Nxd4 a6 *", name: "Sicilian: Kan"}, 
+        {moves: "1. e4 e6 2. d4 d5 *", name: "French Defense"},
+        {moves: "1. e4 b6 2. d4 Bb7 *", name: "Owen's Defense"}
+    ],
+    black: []
+};
